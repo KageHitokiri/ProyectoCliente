cbd6fd0ea48a8346287ca8b8b7be6bdb21b96e99
Carlos <skhcri@gmail.com>
2021-11-12 17:23:39 +0100

    updated functions for html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let player = new Player("Your Name Here",15,5);
+let enemy = new Enemy("Enemy Name",20,3, 2500, 4, 25);
 
 window.addEventListener('load',()=>{    
 
@@ -7,11 +8,28 @@ window.addEventListener('load',()=>{
     //let name = prompt("Introduce un nombre");
     let log = document.getElementById('log');
     document.getElementById('statistics__name').innerHTML += player.getName();    
-    document.getElementById('statistics__currentHP').innerHTML = `Salud: ${player.getHp()} / ${player.getMaxHp()}`;    
-    document.getElementById('hit__button').addEventListener('click',()=>{
-        player.attack();
+    document.getElementById('statistics__currentHP').innerHTML = `Salud: ${player.getHp()} / ${player.getMaxHp()}`;   
+    document.getElementById('statistics__current__Gold').innerHTML += `Oro: ${player.getGold()}`;    
+    document.getElementById('statistics__current__XP').innerHTML += `Experiencia: ${player.getExp()}`;
+    document.getElementById('statistics__current__Potions').innerHTML += `Total de pociones: ${player.getPotions()}`;
+    document.getElementById('enemy__name').innerHTML += enemy.getName();
+    document.getElementById('enemy__current__HP').innerHTML = `Salud: ${enemy.getHp()} / ${enemy.getMaxHp()}`;  
+    document.getElementById('hit_harder__button').addEventListener('click',()=>{
+        player.setPotions(5);
+    })
+    
+    
+
+    document.getElementById('hit__button').addEventListener('click', ()=>{
+        player.attack(enemy);
     });
-    printPlayerHP();
+
+    /*
+    setInterval(()=>{
+        enemy.attack(player);
+    },1000);
+ */
+    updatePlayerHP();
     document.getElementById('flee__button').addEventListener('click',clearTerminal)
     document.getElementById('use__potion').addEventListener('click',()=>{
         player.usePotion();
