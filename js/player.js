77fd3fce69a963ac4b64d154a3484cb657c472c2
Carlos <skhcri@gmail.com>
2021-11-12 17:22:54 +0100

    Added enemy statics

--- js/player.js
+++ js/player.js
@@ -5,13 +5,15 @@ class Player {
         this.maxHp = maxHp;
         this.hp = maxHp;
         this.damage = damage;
+        this.exp = 25;
+        this.gold = 10;
         this.potionCounter = 1;
+        this.isAliveStatus = true;
     }
 
     getName() {
         return this.name;
     }
-
     getMaxHp(){
         return this.maxHp;
     }
@@ -22,6 +24,7 @@ class Player {
     setHp(value){
         this.hp = value;
     }
+
     getDamage(){
         return this.damage;
     }
@@ -32,26 +35,55 @@ class Player {
     getPotions(){
         return this.potionCounter;
     }
-    setPotions(value){
-        this.potionCounter = value;
+    setPotions(potions){
+        console.log(potions);
+        console.log(this.potionCounter);
+        this.potionCounter = potions;
     }
-    test(){
-        console.log(this.damage);
-        printDamage(this.damage,"Aarón");
+    addPotions(value){
+        parseInt(this.potionCounter += value);
     }
-    /**
-     * Añadir el parámetro target, para extraerle tanto el nombre como para aplicarle el daño.
-     * Crear una función para causar el daño al target
-     */
-    attack() {
-        console.log(this.damage);
-        player.hp -= player.damage;
-        printPlayerHP();
 
-        printDamage(this.damage,"Tu enemigo");
-        console.log(player.hp);
+    getGold(){
+        return this.gold;
     }
-    
+    setGold(gold) {
+        this.gold = gold;
+    }
+    addGold(value) {
+        this.gold +=value;
+    }
+
+    getExp() {
+        return this.exp;
+    }
+    setExp(exp){
+        this.exp = exp;
+    }
+    addExp(value) {
+        this.exp += value;
+    }
+
+
+    attack(target) { 
+        let hpLose = target.getHp(); 
+
+        hpLose -= this.damage;  
+        target.setHp(hpLose);
+        updatePlayerHP();
+        updateEnemyHP();
+        printDamage(this.damage,target.getName());  
+
+        if (target.getHp()<=0) {
+            target.isAliveStatus = false;
+            log.value+=`${target.getName()} ha caido.\n`;
+            if (typeof(target==Enemy)) {
+                target.lootMessage();
+                target.lootEnemy(this);
+            }
+        } 
+    }
+
     usePotion(){    
         let health =10;
         console.log(this.potionCounter);
@@ -59,7 +91,8 @@ class Player {
             this.potionCounter--;
             
             player.hp += health;            
-            log.value += `Te sanas ${health} puntos de vida.`        
+            log.value += `Te sanas ${health} puntos de vida.`;
+            updatePotions();        
             if (player.hp > player.maxHp) {
                 player.hp = player.maxHp;
                 log.value+=" Tu salud está al máximo\n";     
@@ -72,13 +105,37 @@ class Player {
         }
         printPlayerHP();
     }
+
+
 }
 
-
 class Enemy extends Player {
-    constructor(name, maxHp, damage, time) {
+    constructor(name, maxHp, damage, time, potions, exp, gold) {
         super(name, maxHp, damage);
         this.time = time
+        this.potionCounter = potions;
+        this.exp = exp
+        this.gold = gold;
+    }
+    
+    lootMessage() {
+        let lootMessage = `Obtienes: \n`;
+        if (this.potionCounter===0) {                        
+        } else if (this.potionCounter===1) {
+            lootMessage += "\t-1 poción";
+        } else {
+            lootMessage += `\t-${this.potionCounter} pociones`;
+        }
+
+        lootMessage += `\n\t-${this.exp} puntos de experiencia\n`;
+        log.value+=lootMessage;        
     }
 
+    lootEnemy(player){
+        
+        player.addExp(this.exp);
+        player.addGold(this.gold);
+        player.addPotions(this.potionCounter);
+        updatePlayerData();
+    }
 }
